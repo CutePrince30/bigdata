85739ff8413004738b0f03d77c0f3fb81cd0d91c
孙杰 <jaysunyun_361@163.com>
2016-07-06 08:55:36 +0800

    update

--- ssp/src/main/java/com/unisk/ad/ssp/aop/logAspect.java
+++ ssp/src/main/java/com/unisk/ad/ssp/aop/logAspect.java
@@ -8,7 +8,6 @@ import com.unisk.ad.ssp.util.UrlUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
